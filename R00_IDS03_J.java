d5ebc905bbd0af143dc884cb9f8aa8a8ab3fd909
cangrew <andresdaviddelgado@gmail.com>
2022-10-11 16:55:51 -0400

    Fix R00

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -28,9 +28,13 @@ public class R00_IDS03_J {
         else loginSuccessful = false;
 
         if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
+            logger.severe("User login succeeded for: " + sanitizeUser(username));
         } else {
-            logger.severe("User login failed for: " + username);
+            logger.severe("User login failed for: " + sanitizeUser(username));
         }
+    }
+
+    public String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9_]+", username) ? username : "unauthorized user";
     }
 }
