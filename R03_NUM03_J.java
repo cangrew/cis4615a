6547be9e8070ad1e1e04e43036392455f6108a34
cangrew <andresdaviddelgado@gmail.com>
2022-10-11 16:35:27 -0400

    functional R03

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,12 +1,17 @@
-import java.io.DataInputStream;
-import java.io.IOException;
+import java.io.*;
 
 //Rule 03. Numeric Types and Operations (NUM)
 //NUM03-J. Use integer types that can fully represent the possible range of unsigned data
 
 public class R03_NUM03_J {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        DataOutputStream jout = new DataOutputStream(new FileOutputStream("test.txt"));
+        jout.writeInt(1898);
+
+        DataInputStream jin = new DataInputStream(new FileInputStream("test.txt"));
+
+        System.out.println(getInteger(jin));
 
     }
 
